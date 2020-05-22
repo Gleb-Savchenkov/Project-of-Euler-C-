27c5ea2bd7a8bce0cc38ec51692fb6219d0d8a3b
Gleb-Savchenkov <64221348+Gleb-Savchenkov@users.noreply.github.com>
2020-05-22 12:27:23 +0300

    Complete Task_2

--- Project_of_Euler/Program.cs
+++ Project_of_Euler/Program.cs
@@ -8,7 +8,7 @@ namespace Project_of_Euler
         {
             Program program = new Program();
 
-            program.Task_1();
+            program.Task_2();
         }
         public void Task_1()
         {
@@ -28,6 +28,35 @@ namespace Project_of_Euler
             }
             Console.WriteLine("Сумма всех чисел до {0} и кратных 3 или 5 = {1}", max, sum);
         }
-        
+        public void Task_2()
+        {
+            /*
+            Каждый следующий элемент ряда Фибоначчи получается при сложении двух предыдущих. Начиная с 1 и 2, первые 10 элементов будут:
+            1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+            Найдите сумму всех четных элементов ряда Фибоначчи, которые не превышают четыре миллиона.
+             */
+
+            int max = 4000000;
+            long sum = 0;
+            int a, b, c;
+            a = 1;
+            b = 2;
+            c = 0;
+            sum += a;
+            sum += b;
+
+            for (int i = 0; i < max; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                sum += c;
+            }
+
+            Console.WriteLine("Сумма все числе Фибоначи до {0} = {1}", max, sum);
+
+
+        }
+
     }
 }
